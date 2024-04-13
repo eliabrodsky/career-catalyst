17916833c11e68cc65e7d01a7abc4b6215fd1fa0
David <davidgarwin@gmail.com>
2024-04-13 00:05:57 +0000

    Adds simple geimini chatbot

---
+++ pages/chatbot.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import google.generativeai as genai
+import json
+
+with st.sidebar:
+    openai_api_key = st.text_input("Gemini API Key", key="chatbot_api_key", type="password")
+
+st.title("💬 Chatbot")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your Gemini API key to continue.")
+        st.stop()
+    genai.configure(api_key=openai_api_key)
+
+    model = genai.GenerativeModel('gemini-pro')
+    
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    prompt = """
+        Below is a json array representing a conversation.                            
+        There are two people speaking, assistant, and user.
+        You are assistant, and your response should be the
+        naural next thing the assitant would say:\n
+        """ + json.dumps(st.session_state.messages)
+    raw = model.generate_content(prompt).text
+    print(raw)
+    try:
+        response = json.loads(raw)
+    except:
+        response = {"role":"assistant","content":raw}
+    st.session_state.messages.append(response)
+    st.chat_message("assistant").write(response['content'])
